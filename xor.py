2bd24184e69facfb496cd0fa2b12bce59d02a9f0
AKushnir <andreykaru2015@yandex.ru>
2019-04-05 23:50:11 +0300

    Commi1

---
+++ xor.py
@@ -0,0 +1,22 @@
+import math
+
+n = int(input())
+s = [int(i) for i in input().split()]
+c = int(input())
+razn = 1000
+razn2 = 1000
+index = None
+for i in range(len(s)):
+    if s[i] == c:
+        print(s[i])
+        index = None
+        break
+    else:
+        razn2 = c - math.fabs(s[i])
+        if razn2 <= 0:
+            continue
+        elif razn2 < razn:
+            razn = razn2
+            index = i
+if index is not None:
+    print(s[index])
